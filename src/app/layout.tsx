573e596d2b1b9c47007f664f5a59bf1ffeaf8bc9
JoeJoeflyn <thaitainguyen336@gmail.com>
2025-03-03 12:30:38 +0700

    feat: add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,17 @@
 import { fetchGlobalData } from '@/api';
 import Footer from '@/components/footer';
 import type { Metadata } from 'next';
-import { Open_Sans } from 'next/font/google';
 import Navbar from '../components/navbar';
+import { openSans } from './fonts';
 import './globals.css';
 
-const openSans = Open_Sans({
-  weight: ['300', '400', '500', '600', '700', '800'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-});
-
 export async function generateMetadata(): Promise<Metadata> {
-  const { data } = await fetchGlobalData();
-
-  const title = data.title;
-  const description = data.description;
-
   return {
-    title: {
-      default: title,
-      template: `%s | ${title}`,
-    },
-    description,
+    title: 'Gaza Soup Kitchen - Humanitarian Food Aid',
+    description:
+      'Providing emergency food relief and support in Gaza. Gaza Soup Kitchen distributes meals to those in need during crisis.',
     applicationName: 'Gaza Soup Kitchen',
-    authors: [
-      {
-        name: 'Gaza Soup Kitchen',
-        url: 'https://gazasoupkitchen.com/about',
-      },
-    ],
+    authors: [{ name: 'Gaza Soup Kitchen' }],
     keywords: [
       'Gaza Food Aid',
       'Humanitarian Relief',
@@ -42,34 +24,12 @@ export async function generateMetadata(): Promise<Metadata> {
       'Aid Organization',
       'Emergency Relief',
     ],
-    openGraph: {
-      title,
-      description,
-      url: 'https://gazasoupkitchen.com',
-      siteName: title,
-      images: [
-        {
-          url: 'https://gazasoupkitchen.com/favicon-32x32.png',
-          alt: 'Gaza Soup Kitchen',
-        },
-      ],
-      locale: 'en_US',
-      type: 'website',
-    },
-    robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        'max-video-preview': -1,
-        'max-image-preview': 'large',
-        'max-snippet': -1,
-      },
-    },
-    category: 'non-profit',
+    generator: 'Gaza Soup Kitchen',
     creator: 'Gaza Soup Kitchen',
     publisher: 'Gaza Soup Kitchen',
+    twitter: {
+      card: 'summary_large_image',
+    },
   };
 }
 
